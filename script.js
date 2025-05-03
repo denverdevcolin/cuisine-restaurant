6406b76e6f23cc75cb06e21ce875e4025e04bc76
Colin Stanford <j.colin.stanford@gmail.com>
2025-05-03 17:07:00 -0600

    testing menu item color toggle

--- script.js
+++ script.js
@@ -13,3 +13,26 @@ hamMenu.addEventListener('click', () => {
 
 // menu highlight //
 
+// onclick, add class for orange text and bottom border
+// remove black border?
+
+function toggleBreakfast() {
+    let element = document.getElementById("breakfast");
+    element.classList.toggle("orange-text-active");
+
+  }
+
+function toggleLunch() {
+    let element = document.getElementById("lunch");
+    element.classList.toggle("orange-text-active");
+}
+
+function toggleDinner() {
+    let element = document.getElementById("dinner");
+    element.classList.toggle("orange-text-active");
+}
+
+function toggleDessert() {
+    let element = document.getElementById("dessert");
+    element.classList.toggle("orange-text-active");
+}
